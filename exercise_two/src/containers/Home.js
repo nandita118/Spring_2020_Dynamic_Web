9bb4c2718ec86a267c6d7e386479eb46f437804e
nandita118 <nk2638@nyu.edu>
2020-02-26 10:05:05 -0500

    UPDATE
    Name update, exercise_two addition

---
+++ exercise_two/src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios"; // abstraction, axios itself is type object
+
+//API Keys
+const defaultKey = "907e284f9bbdde9955ea5e58beec13fe";
+
+function Home(){
+    return (
+
+    )
+}
