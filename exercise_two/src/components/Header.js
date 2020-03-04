c0687333cc672ac7aa0c58a611f0dfeb0156322d
nandita118 <nk2638@nyu.edu>
2020-03-04 08:44:32 -0500

    exercise two pt1

---
+++ exercise_two/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header className="Header">Weather Header</header>
+    );
+}
+
+export default Header;
--- exercise_two/src/containers/Home.js
+++ exercise_two/src/containers/Home.js
@@ -6,6 +6,33 @@ const defaultKey = "907e284f9bbdde9955ea5e58beec13fe";
 
 function Home(){
     return (
+        <div className="Home">
+            <h1>Weather in City</h1>
+            <div className = "WeatherInfo">
 
-    )
+                <div className = "WeatherInfo_Img">
+                    <img src="" alt="" />
+                </div>
+
+                <div className = "WeatherInfo_Data">
+                    <div className="CurrentTemperature">
+                        <p>48&#176;</p>
+                    </div>
+
+                    <div className="OtherTemperatures">
+                        <p>High Temp: <strong>50&#176;</strong></p>
+                        <p>Low Temp: <strong>30&#176;</strong></p>
+                    </div>
+
+                    
+                    <p>Humidity: 50&#176;</p>
+                    <p>Wind: 50&#176;</p>
+
+                </div>
+
+            </div>
+        </div>
+    );
 }
+
+export default Home;
