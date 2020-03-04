13c184c3c3d88b6f507d8b6f23747e41345be16e
nandita118 <nk2638@nyu.edu>
2020-03-04 10:59:03 -0500

    work pt 2

--- exercise_two/src/containers/Home.js
+++ exercise_two/src/containers/Home.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios"; // abstraction, axios itself is type object
 
 //API Keys
 const defaultKey = "907e284f9bbdde9955ea5e58beec13fe";
 
 function Home(){
+
+    const [weatherData, setWeatherData]= useState({});
+    const [city, setCity] = useState(`Seoul`);
+    const [currentTemperature, setCurrentTemperature] = useState('');
+    const [highTemp, setHighTemp] = useState('');
+    const [lowTemp, setLowTemp] = useState('');
+    const [humidity, setHumidity] = useState('');
+    const [wind, setWind] = useState('');
+
+    useEffect(() => {
+        //Make a request for the weather by city
+        axios
+            .get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${defaultKey}`
+            )
+            .then(function(response){
+                //handle success
+                setWeatherData(response.data);
+            })
+            .catch(function(error){
+                //handle error
+                console.log(error);
+            });
+    }, []); //square brackets at the end is called 'hook' - when variables in it, function useEffect will run when any variable changes
+
+    useEffect(() => {
+        if (weatherData.main) {
+            setCurrentTemperature(weatherData.main.temp);
+            setHighTemp(weatherData.main.temp_max);
+            setLowTemp(weatherData.main.temp_min);
+            setHumidity(weatherData.main.humidity);
+            setWind(weatherData.wind.speed);
+        }
+    }, [weatherData]);
+
+    console.log("weatherData", weatherData);
+
     return (
         <div className="Home">
-            <h1>Weather in City</h1>
+            <h1>Weather in {city}</h1>
             <div className = "WeatherInfo">
 
                 <div className = "WeatherInfo_Img">
@@ -16,17 +53,17 @@ function Home(){
 
                 <div className = "WeatherInfo_Data">
                     <div className="CurrentTemperature">
-                        <p>48&#176;</p>
+                        <p>{currentTemperature}&#176;</p>
                     </div>
 
                     <div className="OtherTemperatures">
-                        <p>High Temp: <strong>50&#176;</strong></p>
-                        <p>Low Temp: <strong>30&#176;</strong></p>
+                        <p>High Temp: <strong>{highTemp}&#176;</strong></p>
+                        <p>Low Temp: <strong>{lowTemp}&#176;</strong></p>
                     </div>
 
                     
-                    <p>Humidity: 50&#176;</p>
-                    <p>Wind: 50&#176;</p>
+                    <p>Humidity: {humidity}%</p>
+                    <p>Wind: {wind}mph</p>
 
                 </div>
 
