3d8dfdfc214f597497092879723ddb12c92af310
nandita118 <nk2638@nyu.edu>
2020-03-25 10:39:00 -0400

    commit
    midterm push two - cleaning

--- exercise_three/app.js
+++ exercise_three/app.js
@@ -3,12 +3,12 @@ const express = require('express') //object (essentially everything in js is an 
 const app = express(); //object -- app is the output of function express() -- creating an instance of package express
 const port = 4000; //number variable
 
-//app.get('/', (req, res) => res.send("Hello World")); //function being called
+//app.get('/', (req, res) => res.send("Hello World")); //function being called - flow of files matter
 
 const indexRoute = require('./routes/index.js');
 const aboutRoute = require('./routes/about.js');
 
-// Serve Static Files
+// Serve Static Files -- should be as high as possible
 app.use("/static", express.static("public"));
 // Routing in Express
 app.use("/", indexRoute);
