b11416c57c6e21d91a72543215caaf21e88aefad
nandita118 <nk2638@nyu.edu>
2020-03-27 15:36:25 -0400

    commit
    Run in with CORS error

--- midterm/midterm/src/App.js
+++ midterm/midterm/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//Styles
 import './App.css';
+import Home from "./containers/Home";
 
 function App() {
   return (
-    
+    <div className="SiteWrapper">
+    {/* <Header /> */}
+    <Router>
+     <Switch>
+       <Route path="/">
+         <Home />
+       </Route>
+     </Switch>
+    </Router>
+   
+   </div>
   );
 }
 
--- midterm/midterm/src/containers/Home.js
+++ midterm/midterm/src/containers/Home.js
@@ -2,4 +2,31 @@ import React, {useState, useEffect, Component} from "react";
 import axios from "axios";
 import {useHistory} from "react-router-dom";
 
-const defaultCoronaKey = 
+//const defaultCoronaKey = 
+
+const defaultMTAKey = "ef2bad6edf144d60932c8e1b957eca74"
+
+function Home () {
+    const [mtaData, setMTAData] = useState({});
+   // const [station, setStation] = useState(null);
+
+    let history = useHistory();
+
+    axios
+        .get (
+            `https://datamine.mta.info/mta_esi.php?key=${defaultMTAKey}`
+        )
+        .then(function(response){
+            setMTAData(response.data);
+            console.log(mtaData)
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+
+    return(
+        <h1>It works!</h1>
+    )
+    }
+
+export default Home;
