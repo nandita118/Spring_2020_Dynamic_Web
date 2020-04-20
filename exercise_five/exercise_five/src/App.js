070fdf7af6ade5e2608d83a611b42a9c377b6bc6
nandita118 <nk2638@nyu.edu>
2020-04-20 09:23:36 -0400

    commit
    App.js routing

--- exercise_five/exercise_five/src/App.js
+++ exercise_five/exercise_five/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
+  <div className="App">
+    <Router>
+      <Route exact path="/">
+        <UserProfile />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/create-account">
+        <CreateAccount />
+      </Route>
+    </Router>
+  </div>
     
   );
 }
